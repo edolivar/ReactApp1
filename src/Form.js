d6ce540c670f1ba3f3f9474bcaad947f0b78534f
Edwin Olivares <edolivar@calpoly.edu>
2021-01-06 15:49:35 -0800

    Adding/Deleting Characters is working properly

--- src/Form.js
+++ src/Form.js
@@ -16,6 +16,11 @@ class Form extends Component {
     })
   }
 
+  submitForm = () => {
+      this.props.handleSubmit(this.state)
+      this.setState(this.initialState);
+  }
+
   render() {
     const { name, job } = this.state;
   
@@ -35,7 +40,9 @@ class Form extends Component {
           id="job"
           value={job}
           onChange={this.handleChange} />
+        <input type="button" value="Submit" onClick={this.submitForm} />
       </form>
+      
     );
   }
 }
